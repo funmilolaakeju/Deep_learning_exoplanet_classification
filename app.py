757571fd658e40d3cba7fedd98968c5023361211
funmilolaakeju <fbakeju@gmail.com>
2026-05-02 23:13:36 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -11,60 +11,32 @@ st.set_page_config(
     layout="centered"
 )
 
-
 st.title("Deep Learning Exoplanet Classification")
 
 st.write(
-    "This app predicts whether a Kepler Object of Interest is a "
-    "CANDIDATE, CONFIRMED, or FALSE POSITIVE exoplanet case."
+    "This app predicts whether a Kepler Object of Interest is "
+    "CANDIDATE, CONFIRMED, or FALSE POSITIVE."
 )
 
 
 model = load_model("results/models/exoplanet_mlp_model.keras")
-
 scaler = joblib.load("results/models/scaler.pkl")
-
-label_encoder = joblib.load(
-    "results/models/label_encoder.pkl"
-)
+label_encoder = joblib.load("results/models/label_encoder.pkl")
 
 
 st.subheader("Enter Scientific Features")
 
 koi_period = st.number_input("Orbital Period", value=10.0)
-
 koi_impact = st.number_input("Impact Parameter", value=0.5)
-
 koi_duration = st.number_input("Transit Duration", value=5.0)
-
 koi_depth = st.number_input("Transit Depth", value=500.0)
-
 koi_prad = st.number_input("Planet Radius", value=2.0)
-
 koi_teq = st.number_input("Equilibrium Temperature", value=700.0)
-
 koi_insol = st.number_input("Insolation Flux", value=100.0)
-
-koi_model_snr = st.number_input(
-    "Model Signal-to-Noise Ratio",
-    value=20.0
-)
-
-koi_steff = st.number_input(
-    "Stellar Effective Temperature",
-    value=5500.0
-)
-
-koi_slogg = st.number_input(
-    "Stellar Surface Gravity",
-    value=4.4
-)
-
-koi_srad = st.number_input(
-    "Stellar Radius",
-    value=1.0
-)
-
+koi_model_snr = st.number_input("Model Signal-to-Noise Ratio", value=20.0)
+koi_steff = st.number_input("Stellar Effective Temperature", value=5500.0)
+koi_slogg = st.number_input("Stellar Surface Gravity", value=4.4)
+koi_srad = st.number_input("Stellar Radius", value=1.0)
 
 features = np.array([
     [
@@ -78,31 +50,20 @@ features = np.array([
         koi_model_snr,
         koi_steff,
         koi_slogg,
-        koi_srad
+        koi_srad,
     ]
 ])
 
-
 features_scaled = scaler.transform(features)
 
-
 if st.button("Predict"):
-
     probabilities = model.predict(features_scaled)
-
     predicted_index = np.argmax(probabilities)
-
-    predicted_class = label_encoder.inverse_transform(
-        [predicted_index]
-    )[0]
+    predicted_class = label_encoder.inverse_transform([predicted_index])[0]
 
     st.success(f"Prediction: {predicted_class}")
 
     st.subheader("Class Probabilities")
 
-    for class_name, probability in zip(
-        label_encoder.classes_,
-        probabilities[0]
-    ):
-
+    for class_name, probability in zip(label_encoder.classes_, probabilities[0]):
         st.write(f"{class_name}: {probability:.4f}")
